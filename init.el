a329281a03bb284f60a24b8833765860d1b90512
Sanghyuk Park <sanghyuk.park85@gmail.com>
2025-03-12 10:49:44 -0500

    setting up different configurations depending on different OS

--- init.el
+++ init.el
@@ -3,11 +3,28 @@
 
 (package-initialize)
 
-;; Basic setting
-(set-frame-font "JetBrains Mono 9" nil t)
-;; (set-frame-font "Source Code Pro 10" nil t)
-(set-face-font 'variable-pitch "Roboto Slab-10")
-;; (set-face-font 'variable-pitch "Dejavu Serif-10")
+;; Setting default fonts and file regiesters for different OS
+;; 1. MacOS-specific setup
+(use-package emacs
+  :if (eq system-type 'darwin)
+  :init
+  (set-frame-font "JetBrains Mono 12" nil t)
+  (set-face-font 'variable-pitch "Roboto Slab-12")
+  (set-register ?i '(file . "~/.emacs.d/init.el"))
+  (set-register ?l '(file . "~/.emacs.d/my_loader.org"))
+  (set-register ?k '(file . "~/.emacs.d/my_keybindings.org")))
+
+
+;; 2. Linux-specific setup
+(use-package emacs
+  :if (eq system-type 'gnu/linux)
+  :init
+  (set-frame-font "JetBrains Mono 9" nil t)
+  (set-face-font 'variable-pitch "Roboto Slab-10")
+  (set-register ?i '(file . "~/.config/emacs/init.el"))
+  (set-register ?l '(file . "~/.config/emacs/my_loader.org"))
+  (set-register ?k '(file . "~/.config/emacs/my_keybindings.org")))
+
 
 (put 'dired-find-alternate-file 'disabled nil)
 
@@ -26,4 +43,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+)
